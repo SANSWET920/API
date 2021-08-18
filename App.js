31a4f1c379fdc299960eb65001126724e6c4e040
SANSWET920 <ssanthanam242@gmail.com>
2021-08-18 16:15:34 +0530

    dfvsdfvsdv

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Platform,disableYellowBox } from 'react-native';
+import {  createAppContainer } from 'react-navigation';
+import { createStackNavigator} from "react-navigation-stack";
+
+import * as Permissions from 'expo-permissions';
+import Home from './screens/Home';
+import CreateTask from './screens/CreateTask';
+import TodoStore from './data/TodoStore';
+
+const AppNavigator = createStackNavigator(
+  {
+    Home,
+    CreateTask,
+  },
+  {
+    headerMode: 'none',
+  }
+);
+console.disableYellowBox = true;
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  async componentWillMount() {
+    await this._askForCalendarPermissions();
+    await this._askForReminderPermissions();
+  }
+
+  _askForCalendarPermissions = async () => {
+    await Permissions.askAsync(Permissions.CALENDAR);
+  };
+
+  _askForReminderPermissions = async () => {
+    if (Platform.OS === 'android') {
+      return true;
+    }
+
+    await Permissions.askAsync(Permissions.REMINDERS);
+  };
+
+  render() {
+    return (
+      <TodoStore>
+        <AppContainer />
+      </TodoStore>
+    );
+  }
+}
